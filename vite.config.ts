4f0e80d1c0c4a1a3e2391d4fb8a14c59cb4a7a14
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2026-02-09 21:41:38 +0530

    rollup error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
   define: {
     global: "globalThis",
   },
   
-  plugins: [react(), tailwindcss(), nodePolyfills() as any],
+  plugins: [react(), tailwindcss()],
   optimizeDeps: {
     force: true,
     include: ["react-head", "sockjs-client", "@stomp/stompjs"],
